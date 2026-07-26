b12c67a8e039b2d13583f0df6e3c497f89250bac
phe0nix <starlordindia1230@gmail.com>
2026-07-26 18:29:39 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import './src/loadEnv.js'
 import app from './src/app.js'
+import connectDb from './src/db/index.js'
 
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+
+connectDb().then(async () => {
+    await app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
 })
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose'
+import dns from 'dns'
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);  // force Node to use Google DNS
+dns.setDefaultResultOrder("ipv4first"); 
 
 const connectDb = async () => {
     try {
-
+        const res = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log(`MongoDB Connected: ${res.connection.host}`)
     } catch (e) {
-        next(e)
+        console.log("error while connecting to db: ", error.message);
+        process.exit(1)
     }
 }
+
+export default connectDb
